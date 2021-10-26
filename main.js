b41f7ac9a40d3d454c0ac1ebe37849c1b4bd3e48
annnet7 <51447220+annnet7@users.noreply.github.com>
2021-10-26 17:33:05 +0300

    auto update card text when input changes
    list of connections between card field and form in Object, generate of events of change and focus elements.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const MonthSelectField = document.getElementById('monthNum');
 MonthSelectField.innerHTML = '<option value="" disabled selected>Month</option>';
 for (let i = 1; i <= 12; i++) {
-    MonthSelectField.innerHTML += '<option value="' + i + '">' + i + '</option>';
+    MonthSelectField.innerHTML += '<option value="' + ((i<10)?'0':'') + i + '">' +((i<10)?'0':'') + i + '</option>';
 }
 
 const curDate = new Date().getFullYear();
@@ -23,16 +23,52 @@ cvvField.addEventListener('focusout', () => {
     flipBox.style.transform = 'rotateY(0deg)';
 })
 
+
+//conection of text field and text on the card
+const fieldAndTextCollection = {
+    'cardNumber':'cardNumberText',
+    'cardName':'cardHolderName',
+    'monthNum':'cardMonth',
+    'yearNum':'cardYear',
+    'cvvNum':'cardCVV'
+}
+
+
 //code to make spaces after 4 symbols
 const creditCardField = document.getElementById('cardNumber');
-const cardNumberText = document.getElementById('cardNumberText');
+const cardNumberText = document.getElementById(fieldAndTextCollection['cardNumber']);
 
 function formatCreditCard() {
-    let ccnum = creditCardField.value.replaceAll(/\s/g, '');
+    //remove wrong symbols and spaces
+    let ccnum = creditCardField.value.replaceAll(/\s/g, '').replace(/[^0-9 ]/g, '');
+    //add space after 4 numbers, for empty field return empty string
     creditCardField.value = ccnum ? ccnum.match(/.{1,4}/g).join(' ') : '';
-    return cardNumberText.innerText = creditCardField.value;
+    return true;
 }
 
 creditCardField.addEventListener('change', formatCreditCard);
 creditCardField.addEventListener('input', formatCreditCard);
 creditCardField.addEventListener('keypress', formatCreditCard);
+
+
+
+//lets make the field on card with border when it's changed
+
+
+Object.entries(fieldAndTextCollection).map(function(el) {
+    console.log(el);
+    let inElement = document.getElementById(el[0]);
+    let outElement = document.getElementById(el[1]);
+
+    inElement.addEventListener('focus', function (){
+        outElement.classList.add("accentBorder");
+    });
+    inElement.addEventListener('focusout', function (){
+        outElement.classList.remove("accentBorder");
+    });
+    inElement.addEventListener('input', function (){
+        outElement.innerText = inElement.value;
+    });
+    
+})
+
